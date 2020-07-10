f3a2872646816d0708127bf134bce0b2ea27e375
Vincent Bernardoff <vb@luminar.eu.org>
2020-07-10 12:16:02 +0200

    dune

--- bin/initrip.ml
+++ bin/initrip.ml
@@ -1,5 +1,3 @@
-open Ini
-
 let () =
   let ic = if Array.length Sys.argv > 1 then open_in Sys.argv.(1) else stdin in
-  to_channel stdout (of_channel ic)
+  Ini.to_channel stdout (Ini.of_channel ic)
--- lib/ini.ml
+++ lib/ini.ml
@@ -1,8 +1,5 @@
-open Re_pcre
-open Printf
-
-let section_rex = regexp "[ \t]*\\[[ \t]*([^ \t]+)[ \t]*\\][ \t]*"
-let pair_rex = regexp "[ \t]*([^ \t]+)[ \t]*=[ \t]*([^ \t]+)*[ \t]*"
+let section_rex = Re.Pcre.regexp "[ \t]*\\[[ \t]*([^ \t]+)[ \t]*\\][ \t]*"
+let pair_rex = Re.Pcre.regexp "[ \t]*([^ \t]+)[ \t]*=[ \t]*([^ \t]+)*[ \t]*"
 
 module SM = Map.Make(String)
 
@@ -19,7 +16,7 @@ let of_simple simple =
 let to_simple ini = SM.(bindings (map bindings ini))
 
 let of_channel ic =
-  let open SM in
+  let open Re.Pcre in
   let rec inner sec acc =
     try
       let l = String.trim (input_line ic) in
@@ -27,22 +24,19 @@ let of_channel ic =
       | "" -> inner sec acc
       | s when s.[0] = ';' || s.[0] = '#' -> inner sec acc
       | s when pmatch ~rex:section_rex s -> (* section *)
-        let rex = section_rex in
-        let subs = extract ~rex s in
-        inner subs.(1) (add subs.(1) empty acc)
+        let subs = extract ~rex:(section_rex) s in
+        inner subs.(1) SM.(add subs.(1) empty acc)
       | s when pmatch ~rex:pair_rex s -> (* pairs *)
         let rex = pair_rex in
         let subs = extract ~rex s in
-        let old_map = find sec acc in
-        inner sec (add sec (add subs.(1) subs.(2) old_map) acc)
+        let old_map = SM.find sec acc in
+        inner sec SM.(add sec (add subs.(1) subs.(2) old_map) acc)
       | _ -> acc
     with End_of_file -> acc
-  in inner "" (singleton "" empty)
+  in inner "" SM.empty
 
 let to_channel oc ini =
-  let open SM in
-  iter (fun k v ->
-      if k <> "" then fprintf oc "[%s]\n" k;
-      iter (fun k v -> fprintf oc "%s = %s\n" k v) v;
-      fprintf oc "\n"
+  SM.iter (fun k v ->
+      if k <> "" then Printf.fprintf oc "[%s]\n" k;
+      SM.iter (fun k v -> Printf.fprintf oc "%s = %s\n" k v) v;
     ) ini
